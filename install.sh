6912c7b1a141b86b8a0c9409af09ef0fd005d509
Matt VerLee <mtverlee@mavs.coloradomesa.edu>
2017-03-27 18:57:28 -0600

    Neatness and reliability tweaks,

--- install.sh
+++ install.sh
@@ -1,11 +1,2 @@
-sudo apt-get install livestreamer -y
-sudo wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install termcolor
-pip install requests
-pip install better_exceptions
-sudo rm get-pip.py
-echo "Installation completed!"
+sudo apt-get install livestreamer -y && sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py && pip install virtualenv && virtualenv venv
+source venv/bin/activate && pip install termcolor requests better_exceptions && sudo rm get-pip.py && echo "Installation completed!"
